a782c86825ae78c6a06255ad9da5beb778bd9d8f
SathiyaRam0211 <sathiyaram.0211@gmail.com>
2024-01-23 14:53:31 +0530

    added git ignore

--- app/src/main/java/com/practice/Personal/PersonalApplication.java
+++ app/src/main/java/com/practice/Personal/PersonalApplication.java
@@ -2,12 +2,25 @@ package com.practice.Personal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
+@RestController
 public class PersonalApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PersonalApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalApplication.class, args);
+    }
 
+    @GetMapping("/hello")
+    public Object practice(){
+        Map<String, Object> response = new HashMap<>();
+        response.put("name", "Sathiya");
+        response.put("age", 25);
+        return response;
+    }
 }
